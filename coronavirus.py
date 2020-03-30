007573a70ce851428c7c9482fd29b1f4456dc9dc
Lazar Gugleta <lazar.gugleta@student.tugraz.at>
2020-03-30 10:01:31 +0200

    update for table

--- coronavirus.py
+++ coronavirus.py
@@ -9,41 +9,41 @@ class Coronavirus():
         self.driver = webdriver.Chrome()
 
     def get_data(self):
-        try:
-            self.driver.get('https://www.worldometers.info/coronavirus/')
-            table = self.driver.find_element_by_xpath('//*[@id="main_table_countries"]/tbody[1]')
-            country_element = table.find_element_by_xpath("//td[contains(text(), 'China')]")
-            row = country_element.find_element_by_xpath("./..")
-            data = row.text.split(" ")
-            total_cases = data[1]
-            new_cases = data[2]
-            total_deaths = data[3]
-            new_deaths = data[4]
-            active_cases = data[5]
-            total_recovered = data[6]
-            serious_critical = data[7]
+        #try:
+        self.driver.get('https://www.worldometers.info/coronavirus/')
+        table = self.driver.find_element_by_xpath('//*[@id="main_table_countries_today"]/tbody[1]')
+        country_element = table.find_element_by_xpath("//td[contains(., 'USA')]")
+        row = country_element.find_element_by_xpath("./..")
+        data = row.text.split(" ")
+        total_cases = data[1]
+        new_cases = data[2]
+        total_deaths = data[3]
+        new_deaths = data[4]
+        active_cases = data[5]
+        total_recovered = data[6]
+        serious_critical = data[7]
 
-            #total_cases = row.find_element_by_class_name('sorting_1')
-            #new_cases = row.find_element_by_xpath("//td[3]")
-            #total_deaths = row.find_element_by_xpath("//td[4]")
-            #new_deaths = row.find_element_by_xpath("//td[5]")
-            #active_cases = row.find_element_by_xpath("//td[6]")
-            #total_recovered = row.find_element_by_xpath("//td[7]")
-            #serious_critical = row.find_element_by_xpath("//td[8]")
-            print("Country: " + country_element.text)
-            print("Total cases: " + total_cases)
-            print("New cases: " + new_cases)
-            print("Total deaths: " + total_deaths)
-            print("New deaths: " + new_deaths)
-            print("Active cases: " + active_cases)
-            print("Total recovered: " + total_recovered)
-            print("Serious, critical cases: " + serious_critical)
+        #total_cases = row.find_element_by_class_name('sorting_1')
+        #new_cases = row.find_element_by_xpath("//td[3]")
+        #total_deaths = row.find_element_by_xpath("//td[4]")
+        #new_deaths = row.find_element_by_xpath("//td[5]")
+        #active_cases = row.find_element_by_xpath("//td[6]")
+        #total_recovered = row.find_element_by_xpath("//td[7]")
+        #serious_critical = row.find_element_by_xpath("//td[8]")
+        print("Country: " + country_element.text)
+        print("Total cases: " + total_cases)
+        print("New cases: " + new_cases)
+        print("Total deaths: " + total_deaths)
+        print("New deaths: " + new_deaths)
+        print("Active cases: " + active_cases)
+        print("Total recovered: " + total_recovered)
+        print("Serious, critical cases: " + serious_critical)
 
-            send_mail(country_element.text, total_cases, new_cases, total_deaths, new_deaths, active_cases, total_recovered, serious_critical)
+        send_mail(country_element.text, total_cases, new_cases, total_deaths, new_deaths, active_cases, total_recovered, serious_critical)
 
-            self.driver.close()
-        except:
-            self.driver.quit()
+        self.driver.close()
+        #except:
+        self.driver.quit()
 
 def send_mail(country_element, total_cases, new_cases, total_deaths, new_deaths, active_cases, total_recovered, serious_critical):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -51,7 +51,7 @@ def send_mail(country_element, total_cases, new_cases, total_deaths, new_deaths,
     server.starttls()
     server.ehlo()
 
-    server.login('email', 'password')
+    server.login('lazar.gugleta@gmail.com', 'lafydtbsoiufkwye')
 
     subject = 'Coronavirus stats in your country today!'
 
